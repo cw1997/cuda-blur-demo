634dd94ae262d30efd88b1160a61254ed17ee70d
changwei <changwei1006@gmail.com>
2026-05-21 01:42:19 +0800

    Remove CPU, CUDA, and OpenCL blur implementations; restructure project directories

--- src/blur.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <cstdint>
-
-void blurImageCPU(const uint8_t* input, uint8_t* output, int width, int height, int kernelSize);
-double blurImageGPU(const uint8_t* input, uint8_t* output, int width, int height, int kernelSize);
--- src/bmp.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <cstdint>
-
-void createTestImage(uint8_t* pixels, int width, int height);
-bool readBMP(const char* filename, uint8_t*& pixels, int& width, int& height);
-bool writeBMP(const char* filename, const uint8_t* pixels, int width, int height);
